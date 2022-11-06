6b1bdf52774e576232ca1f09fc18dede7d1c4e19
Ivan Sultanov <Ivan_Sultanov@epam.com>
2022-11-06 17:31:04 +0600

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            string exit;
+
+            do
+            {
+
+                // TODO: Implement the task here.
+                exit = Console.ReadLine();
+            } while (exit != "q");
+        }
+    }
+}
